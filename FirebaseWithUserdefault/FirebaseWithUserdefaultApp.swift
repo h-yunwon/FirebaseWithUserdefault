6d7d8b2d3dcf2f7f3da66415d53dc82bcd604742
h-yunwon <h_ywon@naver.com>
2023-06-12 21:50:44 +0900

    git init

---
+++ FirebaseWithUserdefault/FirebaseWithUserdefaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseWithUserdefaultApp.swift
+//  FirebaseWithUserdefault
+//
+//  Created by Yunwon Han on 2023/06/12.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseWithUserdefaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
